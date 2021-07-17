885096c3fde9c38744b5b859290ddf9150938409
Ricardo <ricardo.chagas@usp.br>
2021-07-17 17:43:44 -0300

    Add logo to home screen

--- lib/modules/login/screens/login_screen.dart
+++ lib/modules/login/screens/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -18,53 +20,62 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Form(
-            key: _formKey,
+            padding: EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: AppLocalizations.of(context)!.username),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return AppLocalizations.of(context)!.emptyField;
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.next,
-                  onEditingComplete: () => node.nextFocus(),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 32),
+                  child: SvgPicture.asset("assets/images/logo.svg"),
                 ),
-                SizedBox(height: 8),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: AppLocalizations.of(context)!.password),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return AppLocalizations.of(context)!.emptyField;
-                    }
-                    return null;
-                  },
-                  textInputAction: TextInputAction.next,
-                  onEditingComplete: () => node.nextFocus(),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!.username),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return AppLocalizations.of(context)!.emptyField;
+                          }
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        onEditingComplete: () => node.nextFocus(),
+                      ),
+                      SizedBox(height: 8),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: AppLocalizations.of(context)!.password),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return AppLocalizations.of(context)!.emptyField;
+                          }
+                          return null;
+                        },
+                        textInputAction: TextInputAction.next,
+                        onEditingComplete: () => node.nextFocus(),
+                      ),
+                      SizedBox(height: 8),
+                      ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              Navigator.popAndPushNamed(context, '/home');
+                            }
+                          },
+                          child: Text(AppLocalizations.of(context)!.login))
+                    ],
+                  ),
                 ),
-                SizedBox(height: 8),
-                ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        Navigator.popAndPushNamed(context, '/home');
-                      }
-                    },
-                    child: Text(AppLocalizations.of(context)!.login))
               ],
-            ),
-          ),
-        ),
+            )),
       ),
     );
   }
